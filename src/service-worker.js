f247aa9fefa16960ca92c39ae66aaccf684c1145
Doug Parker <dgp1130@users.noreply.github.com>
2025-02-11 21:06:53 -0800

    Adds service worker for offline support.

---
+++ src/service-worker.js
@@ -0,0 +1,15 @@
+self.addEventListener('install', (evt) => {
+  evt.waitUntil((async () => {
+    const cache = await caches.open('v1');
+    await cache.addAll(['/']);
+  })());
+});
+
+self.addEventListener('fetch', (evt) => {
+  evt.respondWith((async () => {
+    const cached = await caches.match(evt.request)
+    if (cached) return cached;
+
+    return await fetch(evt.request);
+  })());
+});
